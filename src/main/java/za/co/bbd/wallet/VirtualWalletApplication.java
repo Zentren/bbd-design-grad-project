aeb308538f2caa8dae473db1b40dfc2b200d71f2
Kobus Coetzee <kobus@bbd.co.za>
2019-07-04 17:47:50 +0200

    Rolling file logging system

--- src/main/java/za/co/bbd/wallet/VirtualWalletApplication.java
+++ src/main/java/za/co/bbd/wallet/VirtualWalletApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VirtualWalletApplication {
 
 	public static void main(String[] args) {
-		Logger logger = LoggerFactory.getLogger(VirtualWalletApplication.class);
+		Logger logger = LoggerFactory.getLogger("DB");
 		
 		logger.error("Hello");
 		SpringApplication.run(VirtualWalletApplication.class, args);
