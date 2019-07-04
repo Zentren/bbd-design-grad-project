3b90165d2a9acb422adc26c58ff4147410bd71b0
Andrie <andriesithole@gmail.com>
2019-07-04 18:03:09 +0200

    Added the initial process transaction functionality using the runtime objects.
    -- Issues
        - Objects are recreated per request so they bad for demo purposes. Need to
        find a way to make them global or at most static.

--- src/main/java/za/co/bbd/wallet/api/VirtualWalletController.java
+++ src/main/java/za/co/bbd/wallet/api/VirtualWalletController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -16,30 +17,35 @@ import javax.annotation.PostConstruct;
 import javax.jws.WebMethod;
 import javax.jws.WebResult;
 import javax.jws.WebService;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @RestController("wallet.api.VirtualWalletController")
 @RequestMapping(value = "/wallet/api")
 @WebService(serviceName = "VirtualWallet", name = "VirtualWallet", targetNamespace = "http://virtual.wallet.bbd")
 @Slf4j
 public class VirtualWalletController {
+    private Logger LOGGER = LoggerFactory.getLogger(VirtualWalletController.class);
     private List<User> users = new ArrayList<>();
+    private Map<String, User> MapOfUsers = new HashMap<String,User>();
     private List<Transaction> transactions = new ArrayList<>();
     private List<Account> accounts = new ArrayList<>();
+private Map<String, Account> MapOfAccounts = new HashMap<String,Account>();
+
+    private String ListOfAccounts = "A442233,BU1875,BO1223,A19238,AA6767";
+    private String NamesAndSurnames = "Josh Buhlungu,Percy Rapudi,Gerrit Boshoff";
 
     private Account account;
+    private Boolean initialise = false;
 
     //This will 100% be removed:
     @PostConstruct
     public void fakeDataSetup() {
         account = new Account();
-        account.setAccountName("Spoils of War");
+//        account.setAccountName("Spoils of War");
         account.setAccountNumber("10001284657");
         account.setBalance(1000000);
         account.setAvailableBalance(1000);
-        account.setOverdraftLimit(3000);
+//        account.setOverdraftLimit(3000);
         account.setTransactions(new ArrayList<>());
 
         User user = new User();
@@ -68,6 +74,77 @@ public class VirtualWalletController {
             return null;
         }
     }
+
+    private void initialiseAccounts(){
+        String[] AccountNumbersArray = ListOfAccounts.split(",");
+        Account dummyAccount = null;
+        for(String AccountNumber: AccountNumbersArray){
+            dummyAccount = new Account();
+            dummyAccount.setAccountNumber(AccountNumber);
+            dummyAccount.setBalance(15000);
+            dummyAccount.setAvailableBalance(15000);
+            dummyAccount.setClosed(false);
+            MapOfAccounts.put(dummyAccount.getAccountNumber(), dummyAccount);
+        }
+    }
+
+    private void initialiseUsers(){
+        Random rand = new Random();
+        String[] NamesAndSurnamesArray = NamesAndSurnames.split(",");
+        String[] nameAndTheSurname;
+        User dummyUser = null;
+        for( String NameAndSurname: NamesAndSurnamesArray){
+            nameAndTheSurname = NameAndSurname.split(" ");
+            dummyUser = new User();
+            dummyUser.setFirstName(nameAndTheSurname[0]);
+            dummyUser.setSurname(nameAndTheSurname[1]);
+            dummyUser.setUserId( ""+ rand.nextInt(1000));
+            dummyUser.setPassword("123");
+//            LOGGER.info("The user is: "+ dummyUser.getFirstName());
+            MapOfUsers.put(dummyUser.getFirstName(), dummyUser);
+        }
+
+    }
+
+    @ApiOperation(value = "Authenticate a transaction.", notes = "Authenticate a transaction.")
+    @RequestMapping(value = "/processTransaction/", method = RequestMethod.POST)
+    @WebMethod(operationName = "AuthenticateTransaction")
+    @WebResult(name = "Transaction")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public String processTransaction(
+            @ApiParam(name = "fromAccount", value = "Account number to deduct the funds from.", required = true)
+            @RequestHeader(name = "fromAccount") String fromAccount,
+            @ApiParam(name = "toAccount", value = "Account number to add the funds to.", required = true)
+            @RequestHeader(name = "toAccount") String toAccount,
+            @ApiParam(name = "amountToTransfer", value = "The amount to Transfer between the accounts", required = true)
+            @RequestHeader(name = "amountToTransfer") String amountToTransfer,
+            @ApiParam(name = "firstName", value = "User first name", required = true)
+            @RequestHeader(name = "firstName") String firstName, /*Got to change that to userID*/
+            @ApiParam(name = "password", value = "User Password.", required = true)
+            @RequestHeader(name = "password") String password
+            )
+        {
+
+            if(!initialise){
+                initialiseAccounts();
+                initialiseUsers();
+            }
+            LOGGER.info("Web binding: "+ firstName);
+            LOGGER.info("" + MapOfUsers.containsKey(firstName));
+            LOGGER.info("" + MapOfUsers);
+           if(! MapOfUsers.containsKey(firstName)){
+                return "User is not found";
+           }else if(! MapOfAccounts.containsKey(fromAccount)){
+               return "Account to transfer funds from is not found.";
+           }else if(! MapOfAccounts.containsKey(toAccount)){
+               return "Account to transfer funds to is not found.";
+           }
+
+
+            return "\"Transaction Successful\"";
+        }
+
+
 
     @ApiOperation(value = "Create a new User", notes = "Create a new User")
     @RequestMapping(value = "/user/", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -107,7 +184,7 @@ public class VirtualWalletController {
         }
 
         users.add(user);
-
+        System.out.println("This works");
         return user.getUserId();
     }
 
