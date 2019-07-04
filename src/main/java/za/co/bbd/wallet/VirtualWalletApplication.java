49120b66c9f0303ca76083ef7288e50811d5fc9b
Kobus Coetzee <kobus@bbd.co.za>
2019-07-04 17:01:49 +0200

    Basic configuration of Logger

--- src/main/java/za/co/bbd/wallet/VirtualWalletApplication.java
+++ src/main/java/za/co/bbd/wallet/VirtualWalletApplication.java
@@ -1,5 +1,7 @@
 package za.co.bbd.wallet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VirtualWalletApplication {
 
 	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(VirtualWalletApplication.class);
+		
+		logger.error("Hello");
 		SpringApplication.run(VirtualWalletApplication.class, args);
 	}
 
